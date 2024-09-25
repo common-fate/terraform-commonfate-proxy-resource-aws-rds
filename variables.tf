fa2ed5a0c02a3b0019552e6a3a525b2dfa6a671b
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-09-25 14:46:21 +0800

    Update condition and add comments

--- variables.tf
+++ variables.tf
@@ -31,7 +31,9 @@ variable "rds_security_group_id" {
   default     = ""
 
   validation {
-    condition     = var.create_security_group_rule == true && length(var.rds_security_group_id) == 0
+    # when create_security_group_rule is false, a rds_security_group_id is not required, it can either have a value or be empty
+    # when create_security_group_rule is true, rds_security_group_id must not be empty
+    condition     = var.create_security_group_rule == false || length(var.rds_security_group_id) > 0
     error_message = "rds_security_group_id must not be empty when create_security_group_rule is true."
   }
 }
